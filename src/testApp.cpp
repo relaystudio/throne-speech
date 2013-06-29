7fb9455d38f13731c92aa246d260db5578558da7
Adam Carlucci <carlucci.adam@gmail.com>
2013-06-28 20:44:05 -0400

    remove oF things we're not using

--- src/testApp.cpp
+++ src/testApp.cpp
@@ -169,10 +169,3 @@ int testApp::getReading(int sensor) {
 void testApp::keyPressed(int key){
 
 }
-
-void testApp::keyReleased(int key){ }
-void testApp::mouseMoved(int x, int y ){ }
-void testApp::mouseDragged(int x, int y, int button){ }
-void testApp::mousePressed(int x, int y, int button){ }
-void testApp::mouseReleased(int x, int y, int button){ }
-void testApp::windowResized(int w, int h){ }
--- src/testApp.h
+++ src/testApp.h
@@ -12,12 +12,6 @@ public:
 	void exit();
 	
 	void keyPressed  (int key);
-	void keyReleased(int key);
-	void mouseMoved(int x, int y );
-	void mouseDragged(int x, int y, int button);
-	void mousePressed(int x, int y, int button);
-	void mouseReleased(int x, int y, int button);
-	void windowResized(int w, int h);
 	
 	void setupAudioGraph(string ringToneFile);
 	void setupArduino(int baud);
